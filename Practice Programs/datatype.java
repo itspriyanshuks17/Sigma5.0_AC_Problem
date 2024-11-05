016571f49b539e2b5ae5820c617d9dd78f04429e
Priyanshu K Sharma <153804042+itspriyanshuks17@users.noreply.github.com>
2024-11-05 20:35:27 +0530

    Type Conversion and Type casting

---
+++ Practice Programs/datatype.java
@@ -0,0 +1,17 @@
+
+public class datatype {
+
+	public static void main(String[] args) {
+		
+		//Implicit: Type Conversion
+		int x = 20;
+		float f  = x;
+		System.out.println(f);
+		
+		//Explicit: Type Casting
+		float a = 10.5f;
+		int b = (int)a;
+		System.out.println(b);
+	}
+
+}
