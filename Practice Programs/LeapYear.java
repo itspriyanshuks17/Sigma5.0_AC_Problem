83b249c2d906db79019964bbb4f308f44e53e2d8
Priyanshu K Sharma <153804042+itspriyanshuks17@users.noreply.github.com>
2024-11-06 20:06:54 +0530

    Program to find the given year is leap year or not

---
+++ Practice Programs/LeapYear.java
@@ -0,0 +1,27 @@
+import java.util.*; 
+
+public class LeapYear{
+	public static void main(String[]args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Input the year: ");
+		
+		int year = sc.nextInt(); 
+		
+		boolean x= (year%4) == 0;
+		boolean y= (year%100) != 0; //for non century year(for eg 2024)
+		boolean z= ((year%100==0) && (year%400==0));
+		
+		if(x&& (y||z)) {
+			System.out.println(year+" is a leap year");
+			
+		}
+		
+		else{
+			System.out.println(year+" is not a leap year");
+			
+		}
+		
+	 }
+	
+}
